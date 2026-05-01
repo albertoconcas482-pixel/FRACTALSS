aa1b8bb453420e8c23043554658595a648967d0b
albertoconcas482-pixel <albertoconcas482@gmail.com>
2026-05-01 12:59:29 +0200

    Add documentation comments to fractal_el

--- src/fractal_el.hpp
+++ src/fractal_el.hpp
@@ -1,6 +1,19 @@
 #pragma once
 #include <complex>
 
+/*
+ * Represents a single point in the complex plane,
+ * together with all data produced during fractal
+ * computation and final coloring.
+ *
+ * Fields:
+ *   c          - coordinate in the complex plane
+ *   inside     - true if the point belongs to the Mandelbrot set
+ *   escapeiter - number of iterations before divergence;
+ *                set to maxiter if the point is inside the set
+ *   r, g, b    - final RGB color components (0-255)
+ *                assigned by render_color
+ */
 struct fractal_el {
     std::complex<double> c{};
     bool inside{};
