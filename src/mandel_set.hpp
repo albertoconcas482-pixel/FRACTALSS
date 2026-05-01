44c55eaa5970108808230ab7dc47ab192f4953f1
albertoconcas482-pixel <albertoconcas482@gmail.com>
2026-05-01 13:12:46 +0200

    Read render files

--- src/mandel_set.hpp
+++ src/mandel_set.hpp
@@ -1,16 +1,6 @@
 #pragma once
 #include "fractal_pl.hpp"
 
-/*
- * Mandelbrot set computation module.
- *
- * For each point in the plane, mandel_check determines whether
- * the point belongs to the Mandelbrot set and records the number
- * of iterations before divergence.
- *
- * Results are written directly into the fractal_el elements
- * of the plane (inside, escapeiter).
- */
 namespace mandel_set {
     void mandel_check(fractal_pl& plane, int maxiter);
 }
