1d94a640f91e4bd1fd7043dd2108f4003d017a28
albertoconcas482-pixel <albertoconcas482@gmail.com>
2026-05-02 18:56:27 +0200

    refactor: remove r/g/b from fractal_el, colors now live in render buffer

--- src/fractal_el.hpp
+++ src/fractal_el.hpp
@@ -3,22 +3,19 @@
 
 /*
  * Represents a single point in the complex plane,
- * together with all data produced during fractal
- * computation and final coloring.
+ * together with all data produced during fractal computation.
+ *
+ * RGB color data has been removed: coloring is a presentation
+ * concern handled by render, which fills an external buffer.
  *
  * Fields:
  *   c          - coordinate in the complex plane
  *   inside     - true if the point belongs to the Mandelbrot set
  *   escapeiter - number of iterations before divergence;
  *                set to maxiter if the point is inside the set
- *   r, g, b    - final RGB color components (0-255)
- *                assigned by render_color
  */
 struct fractal_el {
     std::complex<double> c{};
     bool inside{};
     int escapeiter{};
-    unsigned char r{};
-    unsigned char g{};
-    unsigned char b{};
 };
